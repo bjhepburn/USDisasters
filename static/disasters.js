17b6fa07215ccd336adde2a1387b7a610e11c9bf
Bradley Hepburn <119343728+bjhepburn@users.noreply.github.com>
2023-04-18 19:36:38 -0500

    04182023

--- static/disasters.js
+++ static/disasters.js
@@ -12,6 +12,7 @@ let disasters2020 = Object.values(annual['2020']);
 let disasters2021 = Object.values(annual['2021']);
 let disasters2022 = Object.values(annual['2022']);
 let disasters2023 = Object.values(annual['2023']);
+let disObj = {'2016':disasters2016, '2017':disasters2016, '2018':disasters2016, '2019':disasters2016};
 
 // Adding the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -31,46 +32,19 @@ let stateObj = {};
 
 // Functions to update map
 function getColor(d) {
-    return d > 7  ? '#800026' :
-           d > 6  ? '#BD0026' :
-           d > 5  ? '#E31A1C' :
-           d > 4  ? '#FC4E2A' :
-           d > 3  ? '#FD8D3C' :
-           d > 2  ? '#FEB24C' :
-           d > 1  ? '#FED976' :
+    return d > 5  ? '#800026' :
+           d > 4  ? '#BD0026' :
+           d > 3  ? '#E31A1C' :
+           d > 2  ? '#FC4E2A' :
+           d > 1  ? '#FD8D3C' :
+           d > 0  ? '#FEB24C' :
+           d = 0  ? '#FED976' :
                     '#FFEDA0';
 };
-function updateMap(year) {
-    let disasterSet = Object.values(annual[year]);
-    for (i = 0; i < disasterSet.length; i++) {
-        for (j = 0; j < choroData.length; j++) {
-            if (disasterSet[i].stateName == choroData.features.properties.name) {
-                choroData.features.properties = disasterSet[i]
-            }
-        }
-    }
-    let val = document.getElementById("selDisaster");
-    let disaster = val.value;
-    let style = {
-        fillColor: getColor(disaster),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
-        fillOpacity: .7
-    };
-    L.geoJson(choroData,style).addTo(map);
-};
-updateMap('2016');
-
 
 // D3 call & function
-d3.json(test).then(response => {
-    let disasters = response;
-
-    
-    var year = 2016;
-
+d3.json(url).then(response => {
+    let disasters = response.FemaWebDisasterDeclarations;
 
     let select = document.getElementById("selState");
     for (let i = 0; i < stateArray.length; i++) {
@@ -87,38 +61,44 @@ d3.json(test).then(response => {
 
     d3.select("#selYear").on("change", yearChange);
     function yearChange() {
+        document.getElementById('selDisaster').innerText = null;
         let selection = d3.select("#selYear");
         year = selection.property('value');
         updatePie(year);
-        updateMap(year);
+        return(year);    
+    };
+
+
+    function updateMap(disasterObj, disaster) {
+        let disasterSet = disasterObj;
+        let geoData = choroData;
+        for (i = 0; i < disasterSet.length; i++) {
+                if (disasterSet[i].stateName === geoData.features[i].properties.name) {
+                    geoData.features[i].properties = disasterSet[i];
+                }
+            }
+        console.log(geoData);
+
+        let style = {
+            fillColor: getColor(geoData.features.properties[disaster]),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: .7
+            };
+        L.geoJson(geoData,style).addTo(map);
+    };
      
-
-      }
-    
-
-    // function updatePie(year) {
-    //     let disasterCounts = {};
-    //     for (let i = 0; i < disasters.length; i++) {
-    //         if (disasters[i].incidentBeginDate.includes(year)) {
-    //         let disType = disasters[i].incidentType;
-    //         if (disType in disasterCounts) {
-    //             disasterCounts[disType] ++;
-    //         }
-    //         else{
-    //             disasterCounts[disType] = 1;
-    //         }
-    //     }
-    // }
-    //     let trace = [{
-    //         values: Object.values(disasterCounts),
-    //         labels: Object.keys(disasterCounts),
-    //         type: 'pie'
-    //     }];
-    //     let layout = {height: 500, width: 500};
-
-    //     Plotly.newPlot('pie',trace,layout);
-    //     console.log(disasterCounts);
-    // }
+    d3.select("#selDisaster").on("change", updateDisaster);
+    function updateDisaster() {
+                let dropdownMenu = d3.select("#selDisaster");
+                let disasterType = dropdownMenu.property("value");
+                
+                year = yearChange();
+                updateMap(disObj[year], disasterType);
+                return(disasterType);
+    };
 
     function updatePie(year) {
         let disasterCounts = {};
@@ -171,14 +151,17 @@ d3.json(test).then(response => {
 
 
     // Update the scatterplot for state
-    d3.select("#selState").on("change", stateChange);
+    
     function stateChange() {
         let selection = d3.select("#selState");
         state = selection.property('value');
         updatePlot(state);
       }
+
+    d3.select("#selState").on("change", stateChange);
     function updatePlot(state) {
         let yearlyCounts = {'2016':0,'2017':0,'2018':0,'2019':0,'2020':0,'2021':0,'2022':0,'2023':0};
+        
         for (let i = 0; i < disasters.length; i++) {
 
             let year = disasters[i].incidentBeginDate.slice(0,4);
@@ -207,31 +190,8 @@ d3.json(test).then(response => {
             }
         };
         Plotly.newPlot('plot',trace,layout);
-        console.log(disasters);
     }
-
-    let disasterType = d3.select("#selDisaster").on("change", updateDisaster);
-    function updateDisaster() {
-        let dropdownMenu = d3.select("#selDisaster");
-        let disasterType = dropdownMenu.property("value");
-        return(disasterType);
-    }
-
-        
-    //     function updateMapPie(year) {
-    //         for (let i = 0; i < disasters.length; i++) {
-    //             let disaster = disasters[i];
-    //             if (disaster.incidentBeginDate.includes(year)) {
-    //                 yearDisasters.push(disaster);
-    //             };
-            
-    //     }
-        
-    // }
-    // let geojason = L.choropleth(yearDisasters, {
-    //     valueProperty: ""
-    // })
     updatePie(2016);
     updatePlot('AL');
-    updateMap();
+    // updateMap(disObj['2016'],'Fire');
 });
